a7bed13470a18c814db532ab5f15baa1403c8616
Jan Belkner <janbelkner@10clouds.com>
2021-01-05 13:37:42 +0100

    no secrets

--- basync/settings.py
+++ basync/settings.py
@@ -26,7 +26,7 @@ ASYNC = os.environ.get("ASYNC")
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0#(sw&#5j@8qbnnejk)amn0dfh5q7l9u_e%m-)j%)^8--nyc_9"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG") == "True" else False
