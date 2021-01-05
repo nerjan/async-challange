f8ae65bf56dfb9bfbddef6eb1fdc18b7554a01ca
Jan Belkner <janbelkner@10clouds.com>
2021-01-05 13:32:01 +0100

    main

---
+++ async_app/views.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .utils import run_async, run_sync
+
+
+class TheLastOnesView(APIView):
+    def get(self, request):
+        return Response(
+            {
+                "Example post request": "{'async': 'True', 'sync': 'False', 'search_term': 'google'}"
+            }
+        )
+
+    def post(self, request):
+        search_dict = {}
+        for query in request.data["body"]:
+            if settings.ASYNC == "True":
+                search_dict[query] = run_async(query)
+            else:
+                search_dict[query] = run_sync(query)
+        return Response([search_dict])
